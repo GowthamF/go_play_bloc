db5af5f118c5c830501153e52cda984ab97eafaf
GowthamF <40605124+GowthamF@users.noreply.github.com>
2020-04-26 17:19:53 +0530

    changes

--- lib/api/constants.dart
+++ lib/api/constants.dart
@@ -1,5 +1,6 @@
 class Constants {
-  static final String baseUrl = 'http://192.168.8.101';
+  static final String baseUrl =
+      'https://goplayapi20200426045808.azurewebsites.net/';
   static final Map<String, String> headers = {
     'Content-Type': 'application/json'
   };
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,15 +1,22 @@
 class User {
   final int userId;
-  final String userName;
+  final String firstName;
+  final String lastName;
   final String password;
   final String emailAddress;
 
-  User({this.userId = 0, this.userName, this.password, this.emailAddress = ''});
+  User(
+      {this.userId = 0,
+      this.firstName,
+      this.lastName,
+      this.password,
+      this.emailAddress = ''});
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
         userId: json['userId'],
-        userName: json['userName'],
+        firstName: json['userFirstName'],
+        lastName: json['userLastName'],
         password: json['password'],
         emailAddress: json['emailAddress']);
   }
@@ -17,7 +24,8 @@ class User {
   Map<String, dynamic> toJson(User user) {
     return {
       'UserId': user.userId,
-      'UserName': user.userName,
+      'UserFirstName': user.firstName,
+      'UserLastName': user.lastName,
       'Password': user.password,
       'EmailAddress': user.emailAddress
     };
