b43d039ad8dd615ba54ca8808ebfbef9237a6281
GowthamF <40605124+GowthamF@users.noreply.github.com>
2020-04-27 18:56:30 +0530

    adding ratings

--- lib/models/review.dart
+++ lib/models/review.dart
@@ -1,14 +1,14 @@
-import 'package:go_play_bloc/models/models.dart';
-
 class Review {
   final int reviewId;
   final int venueId;
   final String reviewComment;
+  final double ratings;
 
   Review({
     this.reviewId = 0,
     this.venueId,
     this.reviewComment,
+    this.ratings = 0,
   });
 
   factory Review.fromJson(Map<String, dynamic> json) {
@@ -16,6 +16,7 @@ class Review {
       reviewId: json['reviewId'],
       venueId: json['venueId'],
       reviewComment: json['reviewComment'],
+      ratings: json['ratings'].toDouble(),
     );
   }
 
@@ -24,6 +25,7 @@ class Review {
       'ReviewId': review.reviewId,
       'ReviewComment': review.venueId,
       'VenueId': review.venueId,
+      'Ratings': review.ratings
     };
   }
 }
